d89e78946a9f5ea40e5ca3fc5c883084ec1df66b
AlphaSquad <adrifree@googlemail.com>
2017-01-09 14:06:47 +0100

    adapted by_genome evaluation

--- opt/cami-evaluate-binning/bin/genome_bins_2.py
+++ opt/cami-evaluate-binning/bin/genome_bins_2.py
@@ -20,7 +20,7 @@ def get_genome_mapping(mapping_file):
 		genome_id_to_total_length = {}
 		genome_id_to_list_of_contigs = {}
 		anonymous_contig_id_to_genome_id = {}
-		anonymouse_contig_id_to_lengths = {}
+		anonymous_contig_id_to_lengths = {}
 		for line in mapping:
 			if len(line.strip()) == 0 or line.startswith('#'):
 				continue
@@ -32,7 +32,7 @@ def get_genome_mapping(mapping_file):
 				anonymous_contig_id, genome_id, tax_id, contig_id = data
 				total_length = 150. * 2  # doubled because mapping file contains paired reads
 
-			anonymouse_contig_id_to_lengths[anonymous_contig_id] = total_length
+			anonymous_contig_id_to_lengths[anonymous_contig_id] = total_length
 			anonymous_contig_id_to_genome_id[anonymous_contig_id] = genome_id
 			if genome_id not in genome_id_to_total_length:
 				genome_id_to_total_length[genome_id] = 0
@@ -40,7 +40,7 @@ def get_genome_mapping(mapping_file):
 			genome_id_to_total_length[genome_id] += total_length
 			genome_id_to_list_of_contigs[genome_id].append(anonymous_contig_id)
 		mapping.close()
-	return genome_id_to_total_length, genome_id_to_list_of_contigs, anonymous_contig_id_to_genome_id, anonymouse_contig_id_to_lengths
+	return genome_id_to_total_length, genome_id_to_list_of_contigs, anonymous_contig_id_to_genome_id, anonymous_contig_id_to_lengths
 
 
 def read_header(input_stream):
@@ -98,6 +98,41 @@ def read_participant_file(input_stream):
 
 	return read_rows(input_stream, index_key, index_value)
 
+def validate_genomes(file_path_query, file_path_mapping, file_path_output):
+    """
+    This script calculates precision, recall, accuracy and ARI for binned contigs
+    
+    @param file_path_query:
+    @param file_path_mapping:
+    @param file_path_output:
+    @return:
+    """
+    #import ipdb; ipdb.set_trace()
+    genome_id_to_total_length, genome_id_to_list_of_contigs, sequence_id_to_genome_id, anonymouse_contig_id_to_length = get_genome_mapping(file_path_mapping)
+    sequence_id_to_bin_id = {}
+    bin_id_to_list_of_sequence_id = {}
+    bin_id_to_total_length = {}
+    with open(file_path_query) as read_handler:
+        for sequence_id, predicted_bin in read_participant_file(read_handler):
+            sequence_id_to_bin_id[sequence_id] = predicted_bin
+            if predicted_bin not in bin_id_to_total_length:
+                bin_id_to_list_of_sequence_id[predicted_bin] = []
+                bin_id_to_total_length[predicted_bin] = 0
+            bin_id_to_list_of_sequence_id[predicted_bin].append(sequence_id)
+            bin_id_to_total_length[predicted_bin] += anonymouse_contig_id_to_length[sequence_id]
+    
+    #now compute confusion matrix as dictionary of Counters    
+    bin_id_to_genome_id_to_total_length = defaultdict(Counter)
+    for predicted_bin in bin_id_to_list_of_sequence_id:
+        for sequence_id in bin_id_to_list_of_sequence_id[predicted_bin]:
+            genome_id = sequence_id_to_genome_id[sequence_id]
+            bin_id_to_genome_id_to_total_length[predicted_bin][genome_id] += anonymouse_contig_id_to_length[sequence_id]
+
+    (precision,recall) = calcPrecisionRecall(bin_id_to_genome_id_to_total_length, bin_id_to_total_length, genome_id_to_total_length)
+
+    with open(file_path_output + "/by_genome_weighted.tsv", 'w') as write_handler:
+        write_handler.write("Precision\tRecall\n")
+        write_handler.write("%s \t %s\n" % (precision,recall))
 
 def map_genomes(file_path_query, file_path_mapping, file_path_output):
 	"""
@@ -110,7 +145,7 @@ def map_genomes(file_path_query, file_path_mapping, file_path_output):
 	@param file_path_output:
 	@return:
 	"""
-	genome_id_to_total_length, genome_id_to_list_of_contigs, sequence_id_to_genome_id, anonymouse_contig_id_to_lengths = get_genome_mapping(file_path_mapping)
+	genome_id_to_total_length, genome_id_to_list_of_contigs, sequence_id_to_genome_id, anonymous_contig_id_to_lengths = get_genome_mapping(file_path_mapping)
 	sequence_id_to_bin_id = {}
 	bin_id_to_list_of_sequence_id = {}
 	bin_id_to_total_lengths = {}
@@ -121,7 +156,7 @@ def map_genomes(file_path_query, file_path_mapping, file_path_output):
 				bin_id_to_list_of_sequence_id[predicted_bin] = []
 				bin_id_to_total_lengths[predicted_bin] = 0
 			bin_id_to_list_of_sequence_id[predicted_bin].append(sequence_id)
-			bin_id_to_total_lengths[predicted_bin] += anonymouse_contig_id_to_lengths[sequence_id]
+			bin_id_to_total_lengths[predicted_bin] += anonymous_contig_id_to_lengths[sequence_id]
 	bin_metrics = {}
 	bin_id_to_genome_id_to_total_length = {}
 	mapped = set()
@@ -131,7 +166,7 @@ def map_genomes(file_path_query, file_path_mapping, file_path_output):
 			genome_id = sequence_id_to_genome_id[sequence_id]
 			if genome_id not in bin_id_to_genome_id_to_total_length[predicted_bin]:
 				bin_id_to_genome_id_to_total_length[predicted_bin][genome_id] = 0
-			bin_id_to_genome_id_to_total_length[predicted_bin][genome_id] += anonymouse_contig_id_to_lengths[sequence_id]
+			bin_id_to_genome_id_to_total_length[predicted_bin][genome_id] += anonymous_contig_id_to_lengths[sequence_id]
 		max_overlap = 0.0
 		best_genome_id = ""
 		for genome_id in bin_id_to_genome_id_to_total_length[predicted_bin]:
@@ -146,7 +181,7 @@ def map_genomes(file_path_query, file_path_mapping, file_path_output):
 		precision = bin_id_to_genome_id_to_total_length[predicted_bin][best_genome_id] / bin_id_to_total_lengths[predicted_bin]
 		recall = bin_id_to_genome_id_to_total_length[predicted_bin][best_genome_id] / genome_id_to_total_length[best_genome_id]
 		bin_metrics[predicted_bin] = [best_genome_id, precision, recall, genome_id_to_total_length[best_genome_id]]
-	with open(file_path_output, 'w') as write_handler:
+	with open(file_path_output + "/by_genome.tsv", 'w') as write_handler:
 		write_handler.write("Instance\tclass\tprecision\trecall\tpredicted class size\treal class size\n")
 		for predicted_bin in bin_metrics:
 			write_handler.write("strain\t%s\t%s\t%s\t%s\t%s\n" % (
@@ -157,10 +192,16 @@ def map_genomes(file_path_query, file_path_mapping, file_path_output):
 				bin_metrics[predicted_bin][3]))
 		for genid in genome_id_to_list_of_contigs:
 			if genid not in mapped:
-				write_handler.write("strain\t%s\t%s\t%s\t%s\t%s\n" % (genid, 0, 0, 0, genome_id_to_total_length[genid]))
+				write_handler.write("strain\t%s\t%s\t%s\t%s\t%s\n" % (genid, 'NA', 0., 0, genome_id_to_total_length[genid])) #precision is NA for unpredicted bins
 
 
 map_genomes(
+	file_path_query=sys.argv[1],
+	file_path_mapping=sys.argv[2],
+	file_path_output=sys.argv[3]
+	)
+	
+validate_genomes(
 	file_path_query=sys.argv[1],
 	file_path_mapping=sys.argv[2],
 	file_path_output=sys.argv[3]
